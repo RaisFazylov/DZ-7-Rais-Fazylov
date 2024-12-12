7496b701bcd7fff8d24325508f7958d74bca52ba
Раис <@HOME-PC>
2024-12-12 23:32:54 +0300

    Добавьте файлы проекта.

---
+++ DZ 7 Rais Fazylov/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DZ 7 Rais Fazylov")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DZ 7 Rais Fazylov")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("aa6905f5-fb30-4a59-8b2a-78ce92c5c3b2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tumakov Lab/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    public static string ReverseString(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+    public static void ProcessFile(string inputFileName)
+    {
+        if (!File.Exists(inputFileName))
+        {
+            Console.WriteLine($"Файл '{inputFileName}' не существует.");
+            return;
+        }
+        string outputFileName = "OutputFile.txt";
+        try
+        {
+            string content = File.ReadAllText(inputFileName);
+            File.WriteAllText(outputFileName, content.ToUpper());
+            Console.WriteLine($"Содержимое файла '{inputFileName}' успешно записано в '{outputFileName}' в верхнем регистре.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Произошла ошибка при обработке файла: {ex.Message}");
+        }
+    }
+    public static bool ImplementsIFormattable(object obj)
+    {
+        if (obj is IFormattable)
+        {
+            return true;
+        }
+        return false;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Задание 8.2");
+        /* Реализовать метод, который в качестве входного параметра принимает
+        строку string, возвращает строку типа string, буквы в которой идут в обратном порядке.
+        Протестировать метод.
+        */
+        Console.WriteLine("Введите строку для реверса: ");
+        string inputString = Console.ReadLine();
+        string reversedString = ReverseString(inputString);
+        Console.WriteLine($"Обратная строка: {reversedString}");
+        
+        Console.WriteLine("Задание 8.3");
+        /* Написать программу, которая спрашивает у пользователя имя файла. Если
+        такого файла не существует, то программа выдает пользователю сообщение и заканчивает
+        работу, иначе в выходной файл записывается содержимое исходного файла, но заглавными
+        буквами.
+        */
+        Console.WriteLine("\nВведите имя файла для обработки: ");
+        string inputFileName = Console.ReadLine();
+        ProcessFile(inputFileName);
+
+        Console.WriteLine("Задание 8.4");
+        /* Реализовать метод, который проверяет реализует ли входной параметр
+        метода интерфейс System.IFormattable. Использовать оператор is и as. (Интерфейс
+        IFormattable обеспечивает функциональные возможности форматирования значения объекта
+        в строковое представление.)
+        */
+        Console.WriteLine("\nВведите что-нибудь, чтобы проверить на IFormattable: ");
+        string testObject = Console.ReadLine();
+        if (ImplementsIFormattable(testObject))
+        {
+            Console.WriteLine("Объект реализует интерфейс IFormattable.");
+        }
+        else
+        {
+            Console.WriteLine("Объект не реализует интерфейс IFormattable.");
+        }
+    }
+}
